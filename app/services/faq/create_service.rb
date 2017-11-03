3722a368a713cb6507c3706140cb9dcd82facc28
Raphael Onofre <raphaelonofre@gmail.com>
2017-11-02 21:07:53 -0300

    create list, remove and interpret services

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -1,19 +1,24 @@
 module FaqModule
     class CreateService
-        def initialize
-            @company = Company.last
-            @question = params['question-original']
-            @answer = params['answer-original']
-            @hashtags = params['hashtags-original']
+      def initialize(params)
+        # TODO: identify origin and set company
+        @company = Company.last
+        @question = params["question-original"]
+        @answer = params["answer-original"]
+        @hashtags = params["hashtags-original"]
+      end
+  
+      def call
+        if @hashtags == nil || @hashtags == ""
+            return 'Hashtag Obrigatória'
         end
-
-        def call
-            return "Hashtag Obrigatória!" if hashtags == nil
-            Faq.transaction do
-                faq = Faq.create(question: @question, answer: @answer, company: @company)
-                
-            end
-            
+        Faq.transaction do
+          faq = Faq.create(question: @question, answer: @answer, company: @company)
+          @hashtags.split(/[\s,]+/).each do |hashtag|
+            faq.hashtags << Hashtag.create(name: hashtag)
+          end
         end
+        "Criado com sucesso"
+      end
     end
-end
+  end
